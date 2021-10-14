fa1419f9fd455c2185d5dda9ea6c5a485bb42c3f
hup2chris <38256447+hup2chris@users.noreply.github.com>
2021-10-14 23:31:26 +0100

    Update train_data.dart
    gets json from db

--- lib/screens/train_data.dart
+++ lib/screens/train_data.dart
@@ -1,4 +1,4 @@
-//import 'dart:convert';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 //import 'package:html/parser.dart' as parser;
 //import 'package:dio/dio.dart';
@@ -8,16 +8,21 @@ Map<String, int> arrow = {};
 class TrainsData {
   Future getTrainsData() async {
     var decodedData;
+   // List<Map> decodedData = [];
 
     String filej = 'https://hup2.com/flutter/night_trains.php';
 
     http.Response response = await http.get(Uri.parse(filej),);
 
-    /*if (response.statusCode == 200) {
+   // print('response is  ');
+    //print(response.statusCode);
+
+    if (response.statusCode == 200) {
       decodedData = response.body;
-      // decodedData = jsonDecode(response.body);
+     // decodedData = json.decode(utf8.decode(response.bodyBytes));
+      decodedData = jsonDecode(response.body);
       print(decodedData);
-    }*/
+    }
 
     return decodedData;
   }
